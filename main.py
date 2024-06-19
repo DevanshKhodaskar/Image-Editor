6930b7ebfb5d6613f29e8de6329073727ca998a6
DevanshKhodaskar <devanshkhodaskar870@gmail.com>
2024-06-19 17:55:45 +0530

    Functioning of Brightness Is fully Completed  :)

--- main.py
+++ main.py
@@ -23,10 +23,11 @@ mainarea.pack(fill='both', side='right', expand=True)
 mainarea.grid_rowconfigure(0, weight=1)
 mainarea.grid_columnconfigure(0, weight=1)
 
-# Global variables to store images
+# Global variables to store images and widgets
 original_image = None
 displayed_image = None
 photo_label = None
+brightnessScale = None  # Define brightnessScale globally
 
 def open_image():
     path = filedialog.askopenfilename(filetypes=[("Image Files", "*.jpg; *.jpeg; *.png; *.gif; *.bmp")])
@@ -44,7 +45,7 @@ def resize_image(image, max_width, max_height):
             new_width = int(new_height * aspect_ratio)
     else:
         new_height = min(max_height, original_height)
-        new_width = int(new_height * aspect_ratio)
+        new_width = int(new_height / aspect_ratio)
         if new_width > max_width:
             new_width = max_width
             new_height = int(new_width / aspect_ratio)
@@ -52,6 +53,7 @@ def resize_image(image, max_width, max_height):
     return image.resize((new_width, new_height))
 
 def imageShow():
+    global original_image, displayed_image, photo_label
   
     image_path = open_image()
     if image_path:
@@ -77,11 +79,51 @@ def imageShow():
             photo_label.image = new_photo
 
         mainarea.bind('<Configure>', display_image)
- 
 
+def updateBrightness():
+    global brightnessScale, original_image, displayed_image
+    brightImg_container = [None]  # Use a list to store brightImg
+
+    def setBrightness():
+        global original_image, displayed_image, photo_label, brightnessScale
+    
+        # Update the original_image with the new brightness
+        original_image = brightImg_container[0]
+        
+        displayed_image = ImageTk.PhotoImage(original_image)
+        
+        photo_label.config(image=displayed_image)
+        photo_label.image = displayed_image
+        
+        brightnessScale.destroy()
+        applyBrightness.destroy()
+
+    def brightnessControl(value):
+        global displayed_image, photo_label, original_image
+    
+        # Use the original_image for brightness control
+        brightEnhancer = ImageEnhance.Brightness(resize_image(original_image, mainarea.winfo_width(), mainarea.winfo_height()))
+        brightImg = brightEnhancer.enhance(float(value) / 100)
+        
+        # Store brightImg in the container
+        brightImg_container[0] = brightImg
+        
+        # Convert brightImg to PhotoImage
+        bright_photo = ImageTk.PhotoImage(brightImg)
+        
+        # Update the photo_label with the new image
+        photo_label.config(image=bright_photo)
+        photo_label.image = bright_photo
+    
+    brightnessScale = tk.Scale(bottombar, from_=0, to=200, resolution=1, orient='horizontal', length=400, command=brightnessControl)
+    brightnessScale.set(100)
+    brightnessScale.pack()
+
+    applyBrightness = tk.Button(bottombar, text="Apply", command=setBrightness)
+    applyBrightness.pack(side='right')
 
 # Sidebar buttons
-brightness = tk.Button(sidebar, text="Brightness", fg='white', relief='raised', bg='black', borderwidth=2, width=10, height=2, font=('Helvetica', 17))
+brightness = tk.Button(sidebar, text="Brightness", fg='white', relief='raised', bg='black', borderwidth=2, width=10, height=2, font=('Helvetica', 17), command=updateBrightness)
 brightness.grid(column=0, row=0, pady=5)
 
 sharpness = tk.Button(sidebar, text="Sharpness", fg='white', relief='raised', bg='black', borderwidth=2, width=10, height=2, font=('Helvetica', 17))
@@ -101,6 +143,9 @@ vignette.grid(column=0, row=5, pady=5)
 
 shadow = tk.Button(sidebar, text="Shadow", fg='white', relief='raised', bg='black', borderwidth=2, width=10, height=2, font=('Helvetica', 17))
 shadow.grid(column=0, row=6, pady=5)
+
+apply = tk.Button(sidebar, text="Apply", fg='white', relief='raised', bg='black', borderwidth=2, width=10, height=2, font=('Helvetica', 17))
+apply.grid(column=0, row=7, pady=5)
 
 # Topbar buttons
 openButton = tk.Button(topbar, text="Open", fg='white', bg='gray', font=("Helvetica", 14), command=imageShow)
